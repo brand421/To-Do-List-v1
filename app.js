7980fde930d732e019aec79ad99c617b5a7e7b20
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-10 10:13:52 -0400

    add framework and initial structure of project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.get("/", function(req, res) {
+    
+    var today = new Date();
+    var currentDay = today.getDay();
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+    res.render("list", {kindOfDay: daysOfWeek[currentDay]});
+})
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+})
