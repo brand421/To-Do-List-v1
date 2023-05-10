482a7a6e9d5cadea5f94d23cb94c9b3c43f5a453
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-10 13:24:19 -0400

    add list to html and render items array

--- app.js
+++ app.js
@@ -7,14 +7,31 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+var items = [];
+
 app.get("/", function(req, res) {
     
     var today = new Date();
     var currentDay = today.getDay();
     var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
 
+    var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: daysOfWeek[currentDay]});
+    res.render("list", {kindOfDay: day, newListItem: items});
+})
+
+app.post("/", function(req, res) {
+
+    var item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+
 })
 
 
